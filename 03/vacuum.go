7ac40369153fd92f321c2f62e230672fbd032b6d
aaronmckenna <aaronmckennacs@gmail.com>
2021-04-06 18:52:19 -0400

    Update vacuum.go, vacuum_test.go, TEST1
    TEST1 changed in the prompt, so I changed it for Part Two.
    
    In vacuum.go, I changed the house struct, which now includes a string
    determining which Santa is delivering the presents. To follow with
    this change, processInstructions had to be altered as well, as did
    previousHouses so that the houses they had both visited wouldn't
    count twice.
    
    I actually used vacuum_test.go this time, and it was a big help.
    Holds test functions for the three test files (TEST1, TEST2, TEST3).

--- 03/vacuum.go
+++ 03/vacuum.go
@@ -7,8 +7,9 @@ import (
 )
 
 type house struct {
-	x int
-	y int
+	x     int
+	y     int
+	santa string
 }
 
 func getXYFromNextInstr(instr string, currX int, currY int) (int, int) {
@@ -26,17 +27,30 @@ func getXYFromNextInstr(instr string, currX int, currY int) (int, int) {
 	return x, y
 }
 
-func dHouseFunc() house {
-	return house{1, 0}
-}
-
 func processInstructions(instr []string) [8200]house {
 	var houseGrid [8200]house
-	currX := 0
-	currY := 0
-	for i := 0; i < len(instr); i++ {
-		houseGrid[i] = house{currX, currY}
-		currX, currY = getXYFromNextInstr(instr[i], currX, currY)
+	santaX := 0
+	santaY := 0
+	roboX := 0
+	roboY := 0
+	santaX, santaY = getXYFromNextInstr(instr[0], santaX, santaY)
+	roboX, roboY = getXYFromNextInstr(instr[1], roboX, roboY)
+	santa := true
+	for i := 0; i < len(instr)-1; i++ {
+		//fmt.Println(instr[i])
+		if santa {
+			houseGrid[i] = house{santaX, santaY, "santa"}
+			if i+2 < len(instr) {
+				santaX, santaY = getXYFromNextInstr(instr[i+2], santaX, santaY)
+			}
+			santa = false
+		} else {
+			houseGrid[i] = house{roboX, roboY, "robo"}
+			if i+2 < len(instr) {
+				roboX, roboY = getXYFromNextInstr(instr[i+2], roboX, roboY)
+			}
+			santa = true
+		}
 		fmt.Println(houseGrid[i])
 	}
 	return houseGrid
@@ -45,6 +59,8 @@ func processInstructions(instr []string) [8200]house {
 func previousHouses(houseGrid [8200]house, current int) bool {
 	checked := false
 	for i := 0; i < current; i++ {
+		houseGrid[i].santa = "test"
+		houseGrid[current].santa = "test"
 		if houseGrid[i] == houseGrid[current] {
 			checked = true
 			break
@@ -63,8 +79,8 @@ func presentCount(houseGrid [8200]house, instr []string) int {
 	return count
 }
 
-func drive() int {
-	data, err := ioutil.ReadFile("INPUT")
+func drive(filename string) int {
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return -1
 	}
@@ -75,6 +91,6 @@ func drive() int {
 }
 
 func main() {
-	let := drive()
+	let := drive("INPUT")
 	fmt.Println(let)
 }
--- 03/vacuum_test.go
+++ 03/vacuum_test.go
@@ -3,13 +3,22 @@ package main
 import "testing"
 
 func TestTEST1(t *testing.T) {
-	test1Result := drive()
-	if test1Result != 2 {
-		t.Errorf("vacuum.go failed, expected %v, got %v", 2, test1Result)
+	test1Result := drive("TEST1")
+	if test1Result != 3 {
+		t.Errorf("vacuum.go failed, expected %v, got %v", 3, test1Result)
 	}
 }
 
 func TestTEST2(t *testing.T) {
-	test2Result := drive()
+	test2Result := drive("TEST2")
+	if test2Result != 3 {
+		t.Errorf("vacuum.go failed, expected %v, got %v", 3, test2Result)
+	}
+}
 
+func TestTEST3(t *testing.T) {
+	test3Result := drive("TEST3")
+	if test3Result != 11 {
+		t.Errorf("vacuum.go failed, expected %v, got %v", 11, test3Result)
+	}
 }
